0c4c5cd60f109fa368bc3f0c5f448723019e48e7
fred <lemonpaimc@126.com>
2024-11-10 18:37:32 +0800

    first commit

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
+import fingerprint from "@fingerprintjs/fingerprintjs";
+import {localStorageGetItem, localStorageSetItem, sessionStorageGetItem} from "./storage";
+import { AxiosRequestHeaders } from "axios";
 
 export function isPromise(obj?: any): obj is Promise<any> {
   return Object.prototype.toString.call(obj) === '[object Promise]'
@@ -10,4 +13,34 @@ export function parseJson(data: any) {
   } catch (error) {
     return undefined;
   }
+}
+
+export function getToken(key: string) {
+	const local = localStorageGetItem(key, '');
+	if (local) return local;
+	return sessionStorageGetItem(key, '');
+}
+
+export async function getFingerprint() {
+	try {
+		const v = await fingerprint
+			.load();
+		const v_1 = await v.get();
+		localStorageSetItem("fingerprintId", v_1.visitorId);
+		return v_1.visitorId;
+	} catch {
+		return "";
+	}
+}
+
+export function mergeHeaders(headers: Partial<AxiosRequestHeaders>): AxiosRequestHeaders {
+	const accessToken = getToken("access-token");
+	const refreshToken = getToken("refresh-token");
+	if (accessToken) {
+		headers.Authorization = `Bearer ${accessToken}`;
+	}
+	if (refreshToken) {
+		headers["Refresh-Token"] = refreshToken;
+	}
+	return headers as AxiosRequestHeaders;
 }
