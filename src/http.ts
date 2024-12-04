892dd4dbe3ba913670d30b60b8dc07ef551696e5
liuwei <liuwei@zhihe.com>
2024-12-04 10:34:07 +0800

    wip: wip

--- src/http.ts
+++ src/http.ts
@@ -14,16 +14,22 @@ export enum Platform {
 }
 
 export type HttpClientOptions = {
-    platform: Platform;
+    platform?: Platform;
     prefix: string;
-    app: string;
-    version: string;
-    sign: string;
+    app?: string;
+    version?: string;
+    sign?: string;
+}
+
+export type HttpData<D = any> = {
+    code: number;
+    message: string;
+    data?: D;
 }
 
 export abstract class HttpPlugin {
-    abstract request(): Promise<any> | undefined;
-    abstract response(): Promise<any> | undefined;
+    abstract request(config: axios.InternalAxiosRequestConfig<any>): axios.InternalAxiosRequestConfig<any> | Promise<axios.InternalAxiosRequestConfig<any>>;
+    abstract response(value: HttpData, config?: axios.InternalAxiosRequestConfig<any>): Promise<HttpData> | HttpData;
 }
 
 export class HttpClient {
@@ -45,82 +51,107 @@ export class HttpClient {
             withCredentials: true,
             headers: baseHeaders
         });
-        this.instance.interceptors.request.use(
-            (config) => {
-                const { headers } = config;
-                const temp: Partial<AxiosRequestHeaders> = { ...headers };
-                const fingerprintId = localStorageGetItem("fingerprintId", "");
-                const token = getToken('access-token');
-                if (token) {
-                    temp.Authorization = `Bearer ${token}`
-                }
-                if (fingerprintId) {
-                    temp.fingerprint = fingerprintId;
-                    config.headers = mergeHeaders(temp);
-                    return config;
-                }
-                return getFingerprint().then((id) => {
-                    temp.fingerprint = id;
-                    config.headers = mergeHeaders(temp);
-                    return config;
-                });
-            },
-            () => {
-                return reduce(this.plugins, (pre, plugin) => {
-                    return pre.then(plugin.response);
-                }, Promise.reject({ code: 500, message: "请求异常" }));
-            },
-        );
-        this.instance.interceptors.response.use(
-            (value) => {
-                const accessToken = get(value, "headers.access-token", "");
-                const refreshToken = get(value, "headers.refresh-token", "");
-                if (accessToken) {
-                    localStorageSetItem("access-token", accessToken);
-                }
-                if (refreshToken) {
-                    localStorageSetItem("refresh-token", refreshToken);
-                }
-                return get(value, "data", { code: 500, message: "请稍后重试" });
-            },
-            (error) => {
-                Logger.error("HTTP ERROR", error)
-                const res = get(error, "response.data", {
-                    code: 500,
-                    message: "请稍后重试",
-                });
-                return Promise.reject(res);
-            },
-        );
+        this.instance.interceptors.request.use(this.useRequestSuccess,this.useRequestError);
+        this.instance.interceptors.response.use(this.useResponseSuccess, this.useResponseError);
     }
 
-    use(plugin: HttpPlugin) {
+    private getTime() {
+        const now = new Date();
+        const year = now.getFullYear();
+        const month = now.getMonth() + 1; // 月份从 0 开始，所以要加 1
+        const day = now.getDate();
+        const hours = String(now.getHours()).padStart(2, '0');
+        const minutes = String(now.getMinutes()).padStart(2, '0');
+        const seconds = String(now.getSeconds()).padStart(2, '0');
+        return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+    }
+
+    private useRequestError() {
+        return reduce(this.plugins, (pre, plugin) => {
+            return pre.then(plugin.response);
+        }, Promise.reject({ code: 500, message: "请求异常" }));
+    }
+
+    private useRequestSuccess(config: axios.InternalAxiosRequestConfig<any>): axios.InternalAxiosRequestConfig<any> | Promise<axios.InternalAxiosRequestConfig<any>> {
+        const { headers } = config;
+        const temp: Partial<AxiosRequestHeaders> = { ...headers };
+        const fingerprintId = localStorageGetItem("fingerprintId", "");
+        const token = getToken('access-token');
+        if (token) {
+            temp.Authorization = `Bearer ${token}`
+        }
+        if (fingerprintId) {
+            temp.fingerprint = fingerprintId;
+            config.headers = mergeHeaders(temp);
+            return reduce(this.plugins, async (pre, plugin) => {
+                return pre.then(plugin.request);
+            }, Promise.resolve(config));
+        }
+        return reduce(this.plugins, async (pre, plugin) => {
+            return pre.then(plugin.request);
+        }, getFingerprint().then((id) => {
+            temp.fingerprint = id;
+            config.headers = mergeHeaders(temp);
+            return config;
+        }));
+    }
+
+    private useResponseSuccess(value: axios.AxiosResponse<any, any>) {
+        const accessToken = get(value, "headers.access-token", "");
+        const refreshToken = get(value, "headers.refresh-token", "");
+        const url = get(value, "config.url", "");
+        const config = get(value, 'config', {}) as axios.InternalAxiosRequestConfig<any>;
+        if (accessToken) {
+            localStorageSetItem("access-token", accessToken);
+        }
+        if (refreshToken) {
+            localStorageSetItem("refresh-token", refreshToken);
+        }
+        Logger.info(`[${this.getTime()} HTTP SUCCESS]: ${url}`, value);
+        return reduce(this.plugins, (pre, plugin) => {
+            return pre.then((value) => plugin.response(value, config));
+        }, Promise.resolve(get(value, "data", { code: 500, message: "请稍后重试" })));
+    }
+
+    private useResponseError(error: any) {
+        const url = get(error, "config.url", "");
+        Logger.error(`[${this.getTime()} HTTP ERROR]: ${url}`, error);
+        const res = get(error, "response.data", {
+            code: 500,
+            message: "请稍后重试",
+        });
+        return reduce(this.plugins, (pre, plugin) => {
+            return pre.catch((value) => plugin.response(value, error));
+        }, Promise.reject(res))
+    }
+
+    use(plugin: HttpPlugin): HttpClient {
         const item = this.plugins.find((item) => item.constructor === plugin.constructor);
         if (item) return;
         this.plugins.push(plugin);
     }
 
-    get(url: string, config?: axios.AxiosRequestConfig<any>) {
+    get<D = any>(url: string, config?: axios.AxiosRequestConfig<any>): Promise<HttpData<D>> {
         return this.instance.get(url, config);
     }
 
-    post(url: string, data?: any, config?: axios.AxiosRequestConfig<any>) {
+    post<D = any>(url: string, data?: any, config?: axios.AxiosRequestConfig<any>): Promise<HttpData<D>> {
         return this.instance.post(url, data, config);
     }
 
-    delete(url: string, config?: axios.AxiosRequestConfig<any>) {
+    delete<D = any>(url: string, config?: axios.AxiosRequestConfig<any>): Promise<HttpData<D>> {
         return this.instance.delete(url, config);
     }
 
-    put(url: string, data?: any, config?: axios.AxiosRequestConfig<any>) {
+    put<D = any>(url: string, data?: any, config?: axios.AxiosRequestConfig<any>): Promise<HttpData<D>> {
         return this.instance.put(url, data, config);
     }
 
-    head(url: string, config?: axios.AxiosRequestConfig<any>) {
+    head<D = any>(url: string, config?: axios.AxiosRequestConfig<any>): Promise<HttpData<D>> {
         return this.instance.head(url, config);
     }
 
-    options(url: string, config?: axios.AxiosRequestConfig<any>) {
+    options<D = any>(url: string, config?: axios.AxiosRequestConfig<any>): Promise<HttpData<D>> {
         return this.instance.options(url, config)
     }
 }
--- src/logger.ts
+++ src/logger.ts
@@ -2,6 +2,17 @@ function isString(str: any): str is string {
     return Object.prototype.toString.call(str) === '[object String]'
 }
 
+function getTime() {
+  const now = new Date();
+  const year = now.getFullYear();
+  const month = now.getMonth() + 1; // 月份从 0 开始，所以要加 1
+  const day = now.getDate();
+  const hours = String(now.getHours()).padStart(2, '0');
+  const minutes = String(now.getMinutes()).padStart(2, '0');
+  const seconds = String(now.getSeconds()).padStart(2, '0');
+  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+}
+
 export default class Logger {
   static log(label: string, message: string) {
     return console.log(`%c ${label} %c ${message}`, 'background:#000;color:#fff;border-top-left-radius:4px;border-bottom-left-radius:4px;padding:4px', 'background:#ddd;border-top-right-radius:4px;border-bottom-right-radius:4px;padding:4px;color:#000');
